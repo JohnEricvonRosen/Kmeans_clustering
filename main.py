3c99e7854274c7ef191983327b56e3562ac23f02
JohnEricvonRosen <johnericvonrosen@gmail.com>
2019-03-17 16:18:50 +0100

    Completed K-means script.

--- main.py
+++ main.py
@@ -8,17 +8,16 @@ import random as rand
 import itertools
 
 class kmeans:
-    # NOTE: k = number of clusters
-    # NOTE: tolerance = when to stop iterations
-    # NOTE: max_iter = number of iterations max.
+    # k = number of clusters
+    # tolerance = when to stop iterations
+    # max_iter = number of iterations max.
     def __init__(self, k = 3, tolerance = 0.0001, max_iter = 500):
         self.k = k
         self.tolerance = tolerance
         self.max_iter = max_iter
-        self.S = np.array([])
         self.avg = 0.0
     
-    # NOTE: this is read and parse
+    # this is read and parse
     def readfile(self, filename, index):
         values = list()
         lower = index*200
@@ -26,11 +25,18 @@ class kmeans:
         with open(filename, "r") as f:
             for line in itertools.islice(f, lower,upper):
                     values.append(line.split())
-        self.np_array = np.array(values, dtype=np.float64)
+        self.np_array = np.asarray(values, dtype=np.float64)
     
-    #NOTE: splits np_array into K sets    
+    #splits np_array into K sets    
     def chunks(self):
-        self.S = np.split(self.np_array, indices_or_sections = self.k)
+        self.S = []
+        for i in range(0, self.k):
+            self.S.append([])
+        index = 0
+        for chunk in np.array_split(self.np_array, indices_or_sections = self.k):
+            for linechunk in chunk:
+                self.S[index].append(linechunk)
+            index += 1
 
     def compare(self, s2):
         s1 = self.S
@@ -40,15 +46,15 @@ class kmeans:
             if(len(s1[i]) != len(s2[i])):
                 return False
             for j in range(0, len(s2[i])):
-                if(not np.array_equal(s1[i][j], s2[i][j]))
+                if(not np.array_equal(s1[i][j], s2[i][j])):
                     return False
         return True
 
-    #NOTE: computes means for all sets S
+    #computes means for all sets S
     def compute(self):
         iterations = 0
         min = 1000000.0
-        while(flag):
+        while(True):
             sMeans = []
             for k in range(0, self.k):
                 sum_vector = np.zeros((240,), dtype=np.float64)
@@ -57,35 +63,38 @@ class kmeans:
                 sum_vector = np.divide(sum_vector, len(self.S[k]))
                 sMeans.append(sum_vector)
 
-            #NOTE: Initialize S' and calc ||xi-uj||
-            S = np.empty(self.k,200)
-            for j in range(0,200):
+            #Initialize S' and calc ||xi-uj||
+            S = []
+            for i in range(0, self.k):
+                S.append([])
+            for i in range(0,200):
                 sMin = min
                 index = 0
-                for i in range(0, self.k):
-                    distance = np.linalg.norm(self.np_array[i], sMeans[j]
-                    if distance < sMin):
+                for j in range(0, self.k):
+                    distance = np.linalg.norm(self.np_array[i] - sMeans[j])
+                    if distance < sMin:
                         sMin = (distance)
                         index = j
-                S[index].append(self.np_array[i])
+                x = self.np_array[i]
+                S[index].append(x)
         
-        #NOTE: compare S and S'
-        if self.S.compare(S):
-            return self.S
+            #compare S and S'
+            if self.compare(S):
+                return sMeans
 
-        #NOTE: check if sets are empty
-        check = 0
-        while(check < self.k):
-            if(len(S[check]) == 0):
-                del S[check]
-                self.k -= 1
-            check += 1
+            #check if sets are empty
+            check = 0
+            while(check < self.k):
+                if(len(S[check]) == 0):
+                    del S[check]
+                    self.k -= 1
+                check += 1
 
-        #NOTE: set S = S'
-        for i in range(0, self.k):
-            self.S = S[i]
-            iterations += 1
-        return S
+            #set Sj = S'j
+            for i in range(0, self.k):
+                self.S[i] = S[i]
+                iterations += 1
+            return sMeans
 
 def main():
     index = int(input("Input int [0-9]:"))
@@ -96,8 +105,8 @@ def main():
     a.readfile("DigitsBasicRoutines/mfeat-pix.txt", index)
     rand.shuffle(a.np_array)
     a.chunks()
-    S = a.compute()
-
+    codebook = a.compute()
+    print(codebook)
 
 if __name__ == '__main__':
     main()
